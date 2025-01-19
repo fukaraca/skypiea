54cf2c422614465ee0dd587e728030164c12bead
furkan.karaca <furkan.karaca@acronis.com>
2025-01-20 01:23:34 +0300

    some tests

--- internal/server/router.go
+++ internal/server/router.go
@@ -1,13 +1,15 @@
 package service
 
 import (
-	"github.com/fukaraca/skypiea/internal/config"
-	logg "github.com/fukaraca/skypiea/pkg/log"
+	"log/slog"
+	"net/http"
+
 	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
 	sloggin "github.com/samber/slog-gin"
-	"log/slog"
-	"net/http"
+
+	"github.com/fukaraca/skypiea/internal/config"
+	logg "github.com/fukaraca/skypiea/pkg/log"
 )
 
 func NewRouter(cfg *config.Server, logger *slog.Logger, opts ...gin.OptionFunc) *gin.Engine {
@@ -36,12 +38,17 @@ func (s *Server) bindRoutes() {
 	})
 	s.engine.GET("/home", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "base.html", gin.H{
-			"title": "Welcome to Aquila AI",
+			"title": "some titel",
 		})
 	})
-	s.engine.POST("/api/query", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"response": "test reponse.",
+	s.engine.GET("/changepid123", func(c *gin.Context) {
+		counter++
+
+		c.HTML(http.StatusOK, "", gin.H{
+			"field1": "value1",
+			"field2": "value2",
 		})
 	})
 }
+
+var counter int
